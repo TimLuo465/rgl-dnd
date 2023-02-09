dce2e1d44f4a5810974df827b8a4c42ada3fdd50
bin.sun <3210063369@qq.com>
2023-02-09 09:06:07 +0800

    列表支持子组件

--- src/components/Provider.tsx
+++ src/components/Provider.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import LayoutProvider from './LayoutContext';
+
 export default class Provider extends React.PureComponent {
   render() {
     const { children } = this.props;
 
     return (
-      <DndProvider backend={HTML5Backend}>
+      <DndProvider
+        backend={HTML5Backend}
+        options={{ rootElement: document.querySelector('#grid-designer-wrapper') }}
+      >
         <LayoutProvider>{children}</LayoutProvider>
       </DndProvider>
     );
